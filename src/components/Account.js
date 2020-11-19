43ba87153636dd78d850bc600cdafbdda7802d01
mtrinhwincomp <mtrinh11@gmail.com>
2020-11-19 12:39:40 -0800

    working lab

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,14 +3,23 @@ import React, { useState } from 'react'
 const Account = props => {
     let [amount, setAmount] = useState(0)
     let [balance, setBalance] = useState(0)
+    let [action, setAction] = useState()
 
     const handleSubmit = e => {
         e.preventDefault()
-        if (isNaN(amount)) {
+        if (isNaN(amount) || amount < 0) {
             console.log("Not a number")
         }
         else {
-            setBalance(balance + Number(amount))
+            if (action ==='D') {
+                setBalance(balance + Number(amount))
+            } else if (action === 'W') {
+                if ((balance - Number(amount)) >= 0) {
+                    setBalance(balance - Number(amount))
+                } else {
+                    console.log("Insufficent funds, Buddy.")
+                }
+            }
         }
         setAmount(0)
     }
@@ -24,14 +33,16 @@ const Account = props => {
         <div className="account">
             <h2>{props.name}</h2>
             <div className={balanceClass}>${balance}</div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} >
                 <input 
                     type="text" 
                     placeholder="enter an amount" 
                     value={amount} 
                     onChange={ e => setAmount(e.target.value) }
+                    style={{width: '80px'}}
                 />
-                <input type="submit" value="Deposit" />
+                <input type="submit" value="Deposit" onClick={() => setAction("D")}/>
+                <input type="submit" value="Withdraw" onClick={() => setAction("W")}/>
             </form>
         </div>
     )
